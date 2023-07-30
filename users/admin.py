90cadad0a04d4f1eb8b138a73170e7cd3f209032
ksun <ksusha1354@gmail.com>
2023-07-30 08:31:59 +0300

    fixed problem

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from users.models import User
-
+from catalog.models import Product
 
 # Register your models here.
 
-admin.site.register(User)
+
+class ABCAdmin(admin.ModelAdmin):
+    fields = ['description', 'category', 'is_published', ]
+
+    def get_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Модератор"):
+            return ['description', 'category', 'is_published', ]
+        elif request.user.is_superuser:
+            return [i.name for i in Product._meta.fields if i.name != "id"]
+        else:
+            return []
+
+
+
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, ABCAdmin)
